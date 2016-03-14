ef673a0317cc954335454d02745f927682c7a7ff
David Falcón Naranjo <david_el_125@hotmail.com>
2016-03-14 13:54:41 +0000

    to MASTER

--- src/application/db/DataBaseHelper.php
+++ src/application/db/DataBaseHelper.php
@@ -4,6 +4,7 @@ define('MYSQL_USER','kioskon');
 define('MYSQL_PASSWORD','kioskon');
 define('MYSQL_DBNAME','kioskon');
 define('MYSQL_PORT','3306');
+
 define('TABLE_USERS','users');
 define('ID','_id');
 define('USER_NAME','userName');
